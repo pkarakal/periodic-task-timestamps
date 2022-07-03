38b2716e221a3c256511e74a3e831069a8fd2ab6
Pavlos Karakalidis <pavloc.kara@outlook.com>
2022-07-03 16:02:25 +0300

    feat(internal): convert time to string and get time differnce

--- internal/timestamp.go
+++ internal/timestamp.go
@@ -83,10 +83,8 @@ func parse(inp []byte) (time.Time, error) {
 		default:
 			break
 		}
-		fmt.Println(i, char, v)
 		i = i + v
 	}
-	fmt.Println(itemLen)
 
 	switch {
 	case M < 1 || M > 12: // Month 1-12
@@ -137,4 +135,54 @@ func parse(inp []byte) (time.Time, error) {
 
 func ParseString(s string) (time.Time, error) {
 	return parse([]byte(s))
+}
+
+func DateToString(date time.Time) string {
+	return fmt.Sprintf("%d%02d%02dT%02d%02d%02dZ", date.Year(), date.Month(), date.Day(), date.Hour(), date.Minute(), date.Second())
+}
+
+func Diff(a, b time.Time) (year, month, day, hour, min, sec int) {
+	if a.Location() != b.Location() {
+		b = b.In(a.Location())
+	}
+	if a.After(b) {
+		a, b = b, a
+	}
+	y1, M1, d1 := a.Date()
+	y2, M2, d2 := b.Date()
+
+	h1, m1, s1 := a.Clock()
+	h2, m2, s2 := b.Clock()
+
+	year = int(y2 - y1)
+	month = int(M2 - M1)
+	day = int(d2 - d1)
+	hour = int(h2 - h1)
+	min = int(m2 - m1)
+	sec = int(s2 - s1)
+
+	// Normalize negative values
+	if sec < 0 {
+		sec += 60
+		min--
+	}
+	if min < 0 {
+		min += 60
+		hour--
+	}
+	if hour < 0 {
+		hour += 24
+		day--
+	}
+	if day < 0 {
+		// days in month:
+		t := time.Date(y1, M1, 32, 0, 0, 0, 0, time.UTC)
+		day += 32 - t.Day()
+		month--
+	}
+	if month < 0 {
+		month += 12
+		year--
+	}
+	return
 }
--- internal/timestamp_test.go
+++ internal/timestamp_test.go
@@ -31,3 +31,47 @@ func TestShouldReturnError(t *testing.T) {
 		t.Fatalf("Expected month range error. Expected : %v, Got: %v", expected.Error(), got.Error())
 	}
 }
+
+func TestShouldConvertDateToStringWithZeroPad(t *testing.T) {
+	given := time.Date(2020, 01, 31, 03, 03, 03, 0, time.UTC)
+	expected := "20200131T030303Z"
+	got := DateToString(given)
+	if expected != got {
+		t.Fatalf("Expected %s. Got %s", expected, got)
+	}
+}
+func TestShouldConvertDateToStringWithoutZeroPad(t *testing.T) {
+	given := time.Date(2020, 10, 31, 13, 14, 15, 0, time.UTC)
+	expected := "20201031T131415Z"
+	got := DateToString(given)
+	if expected != got {
+		t.Fatalf("Expected %s. Got %s", expected, got)
+	}
+}
+
+func TestShouldGetTimeDifference(t *testing.T) {
+	givenA := time.Date(2020, 10, 31, 13, 14, 15, 0, time.UTC)
+	givenB := time.Date(2021, 11, 12, 15, 16, 17, 0, time.UTC)
+	year, month, day, hour, min, sec := Diff(givenA, givenB)
+	if year != 1 && month != 0 && day != 12 && hour != 2 && min != 2 && sec != 2 {
+		t.Fatalf("Time difference not correct")
+	}
+}
+
+func TestShouldGetTimeDifferenceLeapYear(t *testing.T) {
+	givenA := time.Date(2020, 02, 28, 13, 14, 15, 0, time.UTC)
+	givenB := time.Date(2021, 11, 12, 15, 16, 17, 0, time.UTC)
+	year, month, day, hour, min, sec := Diff(givenA, givenB)
+	if year != 1 && month != 8 && day != 13 && hour != 2 && min != 2 && sec != 2 {
+		t.Fatalf("Time difference not correct")
+	}
+}
+
+func TestShouldGetTimeDifferenceNonLeapYear(t *testing.T) {
+	givenA := time.Date(2021, 02, 28, 13, 14, 15, 0, time.UTC)
+	givenB := time.Date(2022, 11, 12, 15, 16, 17, 0, time.UTC)
+	year, month, day, hour, min, sec := Diff(givenA, givenB)
+	if year != 1 && month != 8 && day != 12 && hour != 2 && min != 2 && sec != 2 {
+		t.Fatalf("Time difference not correct")
+	}
+}
